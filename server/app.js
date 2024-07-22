bcd41561124cc34f0ca0af66f85ab2a9f8953c6f
Gagan Sharma <Gagansharma007>
2024-07-22 22:16:39 +0530

    Backend functionality working

--- server/app.js
+++ server/app.js
@@ -44,3 +44,26 @@ app.get( "/all-news", (req, res) => {
     let url = `https://newsapi.org/v2/everything?q=page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`;
     fetchNews(url,res);
 });
+
+app.options("/top-headlines", cors());
+app.get("/top-headlines", (req ,res)=>{
+    let pageSize = parseInt(req.query.pageSize) || 40;
+    let page = parseInt( req.query.page) || 1;
+    let category = req.query.category || "sports";
+    let url = `https://newsapi.org/v2/top-headlines?category=${category}&language=en&page=${page}&pageSize=${pageSize}&apikey=${API_KEY}`;
+    fetchNews(url,res);
+});
+
+app.options("/country/:iso",cors());
+app.get("/country/:iso", (req, res)=>{
+    let page = parseInt(req.query.page) || 1;
+    let pageSize = parseInt( req.query.pageSize ) || 40;
+    let country = req.params.iso;
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&language=en&page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`;
+    fetchNews(url, res);
+})
+
+const port = process.env.PORT || 3000 ;
+app.listen(port , ()=>{
+    console.log("server started.");
+})
